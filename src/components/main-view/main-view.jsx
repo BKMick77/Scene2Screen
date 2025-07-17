2488bdf874cbabda70d96783e0142e548cfb6ebc
Mike Hyler <mhyler@zoho.com>
2025-07-17 18:48:45 -0400

    Added signup & login views with authentication

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,39 +1,54 @@
 import { useEffect, useState } from 'react'
 import { MovieCard } from "../movie-card/movie-card.jsx";
 import { MovieView } from "../movie-view/movie-view.jsx";
+import { LoginView } from '../login-view/login-view.jsx';
+import { SignupView } from '../signup-view/signup-view.jsx';
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? setToken : null);
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
 
   useEffect(() => {
-    fetch("https://young-tor-59565-22774666cdbf.herokuapp.com/movies")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log('Fetched data:', data);
+    if (!token) {
+      return;
+    }
 
-        const moviesFromApi = data.map((movie) => ({
-          id: movie._id,
-          title: movie.Title,
-          description: movie.Description,
-          director: movie.Director?.Name,
-          genre: movie.Genre?.Name,
-          image: movie.ImagePath
-        }));
-
+    fetch("https://young-tor-59565-22774666cdbf.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => response.json())
+      .then((moviesFromApi) => {
         setMovies(moviesFromApi);
-      })
-      .catch((error) => {
-        console.error("Fetch failed:", error);
       });
-  }, []);
+  }, [token]);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  if (!user) {
+    return (
+      <>
+        login
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or sign up for an account
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      <>
+        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+      </>
     );
   }
 
