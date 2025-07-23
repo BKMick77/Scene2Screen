1daea0945e251bf5e050ac2df94a6e43db4cee9b
Mike Hyler <mhyler@zoho.com>
2025-07-23 14:44:50 -0400

    more debugging

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,29 +1,43 @@
 import PropTypes from "prop-types";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+// export const MovieCard = ({ movie, onMovieClick }) => {
+//   return (
+//     <div
+//       onClick={() => {
+//         onMovieClick(movie);
+//       }}
+//     >
+//       {movie.title}
+//     </div>
+//   );
+// };
+//
+// MovieCard.propTypes = {
+//   movie: PropTypes.shape({
+//     _id: PropTypes.string.isRequired,
+//     Title: PropTypes.string.isRequired,
+//     ImagePath: PropTypes.string,
+//     Description: PropTypes.string,
+//     Director: PropTypes.shape({
+//       Name: PropTypes.string
+//     }),
+//     Genre: PropTypes.shape({
+//       Name: PropTypes.string
+//     })
+//   }).isRequired,
+//   onMovieClick: PropTypes.func
+// };
+//
+
+export const MovieCard = ({ movie }) => {
+  console.log("Rendering MovieCard:", movie);
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.title}
+    <div>
+      <h2>{movie.Title}</h2>
+      <p>ID: {movie._id}</p>
+      <p>Director: {movie.Director?.Name}</p>
+      <p>Genre: {movie.Genre?.Name}</p>
+      <p>{movie.ImagePath}</p>
     </div>
   );
-};
-
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    Title: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string,
-    Description: PropTypes.string,
-    Director: PropTypes.shape({
-      Name: PropTypes.string
-    }),
-    Genre: PropTypes.shape({
-      Name: PropTypes.string
-    })
-  }).isRequired,
-  onMovieClick: PropTypes.func
 };
