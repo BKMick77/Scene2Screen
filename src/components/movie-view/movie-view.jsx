9bd700555a6a7d87025b4342cd86df040598817d
Mike Hyler <mhyler@zoho.com>
2025-07-24 11:39:05 -0400

    updated image path

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,26 +5,29 @@ export const MovieView = ({ movie, onBackClick }) => {
     <div>
 
       <div>
-        <img src={movie.image} />
-      </div>
+        <img
+          src={movie.ImagePath}
+          alt={movie.Title}
+          style={{ width: "300px", height: "auto", display: "block", marginBottom: "1rem" }}
+        />     </div>
 
       <div>
         <span> Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
 
       <div>
         <span> Description: </span>
-        <span>{movie.description}</span>
+        <span>{movie.Description}</span>
       </div>
 
       <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.Director?.Name}</span>
       </div>
 
       <div> <span>Genre: </span>
-        <span>{movie.genre}</span>
+        <span>{movie.Genre?.Name}</span>
       </div>
 
       <div> <button onClick={onBackClick}>Back</button>
