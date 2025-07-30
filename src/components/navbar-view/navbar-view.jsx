103609d5ddc681648010ea5c16e25d8c5cc63153
Mike Hyler <mhyler@zoho.com>
2025-07-30 17:03:18 -0400

    refactored to links

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,17 +1,24 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const NavbarView = ({ user, onHome, onLogout }) => {
+export const NavbarView = ({ user, onLogout }) => {
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg" sticky="top">
       <Container fluid>
-        <Navbar.Brand href="#">myFlix</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">myFlix</Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar-nav" />
         <Navbar.Collapse id="main-navbar-nav">
-          <Nav className="auto">
-            {user && (
+          <Nav className="me-auto">
+            {!user ? (
               <>
-                <Nav.Link onClick={onHome}>Home</Nav.Link>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link onClick={onLogout}>Logout</Nav.Link>
+                <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
               </>
             )}
           </Nav>
