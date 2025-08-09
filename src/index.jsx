3c5de46f9f49ee4d106145ec5877fb85d3888f65
Mike Hyler <mhyler@zoho.com>
2025-08-09 17:15:41 -0400

    reformated corrected conform.lua

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,12 @@
-import { createRoot } from "react-dom/client";
-import { MainView } from "./components/main-view/main-view";
-import "./index.scss";
-
+import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
+import './index.scss';
 
 const MyFlixApplication = () => {
-  return (
-    <MainView />
-  );
+  return <MainView />;
 };
 
-const container = document.querySelector("#root");
+const container = document.querySelector('#root');
 const root = createRoot(container);
 
 root.render(<MyFlixApplication />);
